61384dc9b9d071cc75c763c02327883f0c98ac9b
dadrai <90080470+dadrai@users.noreply.github.com>
2022-12-13 06:33:54 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import Clock from './components/Clock';
+import 'animate.css';
+
+function App(props) {
+  // const timer = function(){
+  //   let time = new Date();
+  //   return
+
+ 
+    
+  // };
+
+  // return timer();
+  return <Clock date={new Date()}/>;
+}
+
+export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
